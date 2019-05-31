6cd4e611014c38b54138fb95fba8bbf2042a0302
nbozovic <bozovic.nikola@yandex.ru>
2019-05-31 13:25:44 +0200

    Update C_Administrator.php

--- Implementacija/application/controllers/C_Administrator.php
+++ Implementacija/application/controllers/C_Administrator.php
@@ -1,18 +1,20 @@
 <?php
+
 include_once('C_Zajednicki.php');
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
- * Description of C_Administrator
+ * Kontroler za Administratora
  *
- * @author Nikola
+ * @author Nikola Bozovic 0439/2016
+ * @version 1.0
  */
 class C_Administrator extends C_Zajednicki {
 
+    /**
+     * Kreiranje nove instance
+     * 
+     * @return void
+     */
     public function __construct() {
         parent::__construct();
 
@@ -33,11 +35,18 @@ class C_Administrator extends C_Zajednicki {
         }
     }
 
+    /**
+     * Pocetna stranica
+     * 
+     * @param string $poruka 
+     * 
+     * @return void
+     */
     public function index($poruka = null) {
         $topJeloId = $this->M_Recenzija->dohvatiTopJelo();
         $recenzija = $this->M_Recenzija->dohvatiTopRecenziju($topJeloId->IdJelo);
         $jelo = $this->M_Jelo->dohvatiJelo($topJeloId->IdJelo);
-        
+
         $klasa = new stdClass();
         $klasa->Putanja = $this->M_Slika->dohvatiPutanju($jelo->IdSlika)->Putanja;
         $klasa->IdJelo = $topJeloId->IdJelo;
@@ -47,12 +56,17 @@ class C_Administrator extends C_Zajednicki {
         $gurman = $this->M_Gurman->dohvatiGurmana($recenzija->IdKorisnik);
         $klasa->kIme = $gurman->KorisnickoIme;
         $klasa->idK = $gurman->IdKorisnik;
-        
+
         $this->load->view('sablon/headerAdmin.php', ['title' => 'Pretraga']);
         $this->load->view('stranice/main.php', ['poruka' => $poruka, 'jelo' => $klasa]);
         $this->load->view('sablon/footer.php');
     }
 
+    /**
+     * Prikaz stranice za upravljanje dodatim jelima
+     * 
+     * @return void
+     */
     public function upravljanjeJelima() {
         $jela = $this->M_Jelo->dohvatiNepregledanaJela();
         $sastojci = NULL;
@@ -68,56 +82,95 @@ class C_Administrator extends C_Zajednicki {
         $this->load->view('sablon/footer.php');
     }
 
+    /** Prikaz stranice za upravljanje dodatim recenzijama
+     * 
+     * @return void
+     */
     public function upravljanjeRecenzijama() {
         $recenzije = $this->M_Recenzija->dohvatiNepregledaneRecenzije();
         $this->load->view('sablon/headerAdmin.php', ['title' => 'Upravljanje Recenzijama']);
         $this->load->view('stranice/upravljanjeRecenzijama.php', ['recenzije' => $recenzije]);
         $this->load->view('sablon/footer.php');
     }
-    
+
+    /** Prikaz stranice za upravljanje novim  registaracijama
+     * 
+     * @return void
+     */
     public function upravljanjeRegistracijama() {
         $registracije = $this->M_Restoran->dohvatiNepregledaneRegistracije();
         $this->load->view('sablon/headerAdmin.php', ['title' => 'Upravljanje Registracijama']);
         $this->load->view('stranice/upravljanjeRegistracijama.php', ['registracije' => $registracije]);
         $this->load->view('sablon/footer.php');
     }
-    
+
+    /** Obrada pregledanog jela
+     * 
+     * @return void
+     */
     public function pregledanoJelo() {
         $id = $this->input->get('id');
         $this->M_Jelo->postaviPregledano($id);
         $this->upravljanjeJelima();
     }
 
+    /** Brisanje pregledanog jela
+     * 
+     * @return void
+     */
     public function obrisiJelo() {
         $id = $this->input->get('id');
         $this->M_Jelo->obrisiJelo($id);
         $this->upravljanjeJelima();
     }
-     public function pregledanaRecenzija() {
+
+    /** Obrada pregledane recenzije
+     * 
+     * @return void
+     */
+    public function pregledanaRecenzija() {
         $idk = $this->input->get('idk');
         $idj = $this->input->get('idj');
-        $this->M_Recenzija->postaviPregledano($idk,$idj);
+        $this->M_Recenzija->postaviPregledano($idk, $idj);
         $this->upravljanjeRecenzijama();
     }
 
+    /** Brisanje pregledane recenzije
+     * 
+     * @return void
+     */
     public function obrisiRecenziju() {
         $idk = $this->input->get('idk');
         $idj = $this->input->get('idj');
-        $this->M_Recenzija->obrisiRecenziju($idk,$idj);
+        $this->M_Recenzija->obrisiRecenziju($idk, $idj);
         $this->upravljanjeRecenzijama();
     }
+
+    /** Obrada pregledane registracije
+     * 
+     * @return void
+     */
     public function pregledanaRegistracija() {
         $id = $this->input->get('id');
         $this->M_Restoran->postaviPregledano($id);
         $this->upravljanjeRegistracijama();
     }
 
+    /** Brisanje pregledane registracije
+     * 
+     * @return void
+     */
     public function obrisiRegistraciju() {
         $id = $this->input->get('id');
         $this->M_Restoran->obrisiRegistraciju($id);
         $this->upravljanjeRegistracijama();
     }
 
+    /**
+     * Sluzi da se Administrator izloguje
+     * 
+     * @return void
+     */
     public function izlogujSe() {
         $korisnik = new stdClass();
         $korisnik->tipKorisnika = 'gost';
@@ -125,6 +178,37 @@ class C_Administrator extends C_Zajednicki {
         redirect('C_Gost');
     }
 
+    /**
+     * Funkcija za ispis kontakt informacija
+     * 
+     * @return void
+     */
+    public function kontakt() {
+
+        $this->load->view('sablon/headerAdmin.php', ['title' => 'Kontakt']); //ako je gurman ulogovan
+        $this->load->view('stranice/kontakt.php');
+        $this->load->view('sablon/footer.php');
+    }
+
+    /**
+     * Funkcija za ispis informacija o nama
+     * 
+     * @return void
+     */
+    public function onama() {
+
+        $this->load->view('sablon/headerAdmin.php', ['title' => 'O nama']); //ako je gurman ulogovan
+        $this->load->view('stranice/onama.php');
+        $this->load->view('sablon/footer.php');
+    }
+
+    /**
+     * Prikaz profila Gurmana
+     * 
+     * @param int $idGurman
+     * 
+     * @return void
+     */
     function pregledProfilaGurmana($idGurman) {
         $gurman = $this->M_Gurman->dohvatiGurmana($idGurman);
         $recenzije = $this->M_Recenzija->dohvatiRecenzijeGurmana($idGurman);
@@ -143,56 +227,130 @@ class C_Administrator extends C_Zajednicki {
         $this->load->view('stranice/pregledGurmana.php', $info);
         $this->load->view('sablon/footer.php');
     }
+
+    /**
+     * Prikaz jela
+     * 
+     * @param int $id -> idJelo
+     * 
+     * @return void
+     */
     public function prikaziJelo($id) {
         $info = parent::prikaziJelo($id);
-        
+
         $this->load->view('sablon/headerAdmin.php', ['title' => $info['jelo']->Naziv]);
         $this->load->view("stranice/prikazJela.php", $info);
         $this->load->view('sablon/footer.php');
     }
-    
+
+    /**
+     * Prikaz rezultata pretrage jela po nazivu
+     * 
+     * @param string $val
+     * 
+     * @return void
+     */
     public function pretragaJelaPoNazivu($val) {
         $info = parent::pretragaJelaPoNazivu($val);
         $info['naslov'] = 'Rezultat pretrage';
-        
+
         $this->load->view('sablon/headerAdmin.php', ['title' => 'Rezultat pretrage']);
         $this->load->view("stranice/rezultatPretrage.php", $info);
         $this->load->view('sablon/footer.php');
     }
-    
+
+    /**
+     * Prikaz rezultata pretrage jela po sastojku
+     * 
+     * @param string $val
+     * 
+     * @return void
+     */
     public function pretragaJelaPoSastojku($val) {
         $info = parent::pretragaJelaPoSastojku($val);
         $info['naslov'] = 'Rezultat pretrage';
-        
+
         $this->load->view('sablon/headerAdmin.php', ['title' => 'Rezultat pretrage']);
         $this->load->view("stranice/rezultatPretrage.php", $info);
         $this->load->view('sablon/footer.php');
     }
-    
+
+    /**
+     * Prikaz rezultata pretrage jela po restoranu
+     * 
+     * @param string $val
+     * 
+     * @return void
+     */
     public function pretragaJelaPoRestoranu($val) {
         $info = parent::pretragaJelaPoRestoranu($val);
         $info['naslov'] = 'Rezultat pretrage';
-        
+
         $this->load->view('sablon/headerAdmin.php', ['title' => 'Rezultat pretrage']);
         $this->load->view("stranice/rezultatPretrage.php", $info);
         $this->load->view('sablon/footer.php');
     }
-    
+
+    /**
+     * Prikaz rezultata pretrage restorana po nazivu
+     * 
+     * @param string $val
+     * 
+     * @return void
+     */
     public function pretragaRestoranaPoNazivu($val) {
         $info = parent::pretragaRestoranaPoNazivu($val);
         $info['naslov'] = 'Rezultat pretrage';
-        
+
         $this->load->view('sablon/headerAdmin.php', ['title' => 'Rezultat pretrage']);
         $this->load->view("stranice/rezultatPretrageRestoran.php", $info);
         $this->load->view('sablon/footer.php');
     }
-    
+
+    /**
+     * Prikaz rezultata pretrage restorana po adresi
+     * 
+     * @param string $val
+     * 
+     * @return void
+     */
     public function pretragaRestoranaPoAdresi($val) {
         $info = parent::pretragaRestoranaPoAdresi($val);
         $info['naslov'] = 'Rezultat pretrage';
-        
+
         $this->load->view('sablon/headerAdmin.php', ['title' => 'Rezultat pretrage']);
         $this->load->view("stranice/rezultatPretrageRestoran.php", $info);
         $this->load->view('sablon/footer.php');
     }
+
+    /**
+     * Prikaz profila restorana
+     * 
+     * @param int $idRestorana
+     * 
+     * @return void
+     */
+    public function pregledRestorana($idRestorana) {
+        $info = parent::pregledRestorana($idRestorana);
+
+        $this->load->view('sablon/headerAdmin.php', ['title' => 'Rezultat pretrage']);
+        $this->load->view("stranice/pregledRestorana.php", $info);
+        $this->load->view('sablon/footer.php');
+    }
+
+    /**
+     * Prikaz menija restorana
+     * 
+     * @param int $idRestorana
+     * 
+     * @return void
+     */
+    public function prikaziMeniRestorana($idRestorana) {
+        $info = parent::prikaziMeniRestorana($idRestorana);
+
+        $this->load->view('sablon/headerAdmin.php', ['title' => 'Rezultat pretrage']);
+        $this->load->view("stranice/meniRestorana.php", $info);
+        $this->load->view('sablon/footer.php');
+    }
+
 }
